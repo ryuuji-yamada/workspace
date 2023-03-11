d590a9c72ee1938288462b4175f628a90a4932bd
cassandra <yamada0525@outlook.jp>
2023-03-11 23:17:19 +0900

    return user class's paramaters

--- Project(BankingApp)/ClickerEmpireGame.js
+++ Project(BankingApp)/ClickerEmpireGame.js
@@ -17,11 +17,18 @@ class User{//完了
     }
 }
 
-/*
-class Items{
-
+class Items{//完了
+    constructor(name, type, currentAmount, maxAmount, perMoney, perRate, price, url){
+        this.name = name;
+        this.type = type;
+        this.currentAmount = currentAmount;
+        this.maxAmount = maxAmount;
+        this.perMoney = perMoney;
+        this.perRate = perRate;
+        this.price = price;
+        this.url = url;
+    }
 }
-*/
 
 class View{
     static createInitialPage(){//完了
@@ -50,7 +57,7 @@ class View{
         `;
         return config.initialPage.append(container);//<div id="initialPage">の中にcontainerの内容を入れ込む
     }
-    static createMainPage(){/*未実装の内容=>displayItems/reset/save*/
+    static createMainPage(user){/*未実装の内容=>displayItems/reset/save*/
         let container = document.createElement("div");
         container.innerHTML =
         `
@@ -76,19 +83,19 @@ class View{
             </div>
         </div>
         `
-        container.querySelectorAll("#burgerStatus")[0].append(View.createBurgerStatus());//burgerStatus作成
-        container.querySelectorAll("#userInfo")[0].append(View.createUserInfo());//userInfo作成
+        container.querySelectorAll("#burgerStatus")[0].append(View.createBurgerStatus(user));//burgerStatus作成
+        container.querySelectorAll("#userInfo")[0].append(View.createUserInfo(user));//userInfo作成
         container.querySelectorAll("#displayItems")[0].append(View.createItemsPage());//displayItems作成
         return container;
     }
 
-    static createBurgerStatus(){//未実装=>user情報の反映//ハンバーガークリック操作
+    static createBurgerStatus(user){//未実装=>ハンバーガークリック操作
         let container = document.createElement("div");
         container.innerHTML =
         `
         <div class="bg-navy text-white text-center">
             <h5>sample Burgers</h5>
-            <p>one click ￥sample </p>
+            <p>one click ￥${user.incomePerClick} </p>
         </div>
         <div class="p-2 pt-5 d-flex justify-content-center">
             <img src="https://cdn.pixabay.com/photo/2014/04/02/17/00/burger-307648_960_720.png" width=80% class="py-2 hover img-fuid" id="burger">
@@ -98,22 +105,22 @@ class View{
         return container;
     }
 
-    static createUserInfo(){//未実装=>user情報の反映
+    static createUserInfo(user){//完了
         let container = document.createElement("div");
         container.classList.add("d-flex", "flex-wrap", "p-1");
         container.innerHTML =
         `
         <div class="text-white text-center col-12 col-sm-6 userInfoBorder">
-            <p>sample.name</p>
+            <p>${user.name}</p>
         </div>
         <div class="text-white text-center col-12 col-sm-6 userInfoBorder">
-            <p>sample.years old</p>
+            <p>${user.age} years old</p>
         </div>
         <div class="text-white text-center col-12 col-sm-6 userInfoBorder">
-            <p>sample.days</p>
+            <p>${user.days} days</p>
         </div>
         <div class="text-white text-center col-12 col-sm-6 userInfoBorder">
-            <p>￥sample.money</p>
+            <p>￥${user.money}</p>
         </div>
         `
         return container;
@@ -172,11 +179,17 @@ class View{
 class Controller{
     //timer;
 
-    static startGame(){//未実装=>Loginボタンクリック時の詳細な処理//newボタンクリック時の詳細な処理
+    static startGame(){//未実装=>Loginボタンクリック時の詳細な処理
         View.createInitialPage();
         let newGameBtn = config.initialPage.querySelectorAll("#newGame")[0];
         newGameBtn.addEventListener("click",function(){
-            Controller.moveInitialToMain();
+            let userName = config.initialPage.querySelectorAll("input")[0].value;//InitialPageで入力された値をuserNameとして設定
+            if(userName == ""){
+                alert("名前を入力してください");
+            }else{
+                let user = Controller.createInitialUserAccount(userName);
+                Controller.moveInitialToMain(user);
+            }
         });
         let loginBtn = config.initialPage.querySelectorAll("#login")[0];
         loginBtn.addEventListener("click",function(){
@@ -184,16 +197,30 @@ class Controller{
         });
     }
 
-    static moveInitialToMain(){//未実装=>タイマー開始の処理
+    static moveInitialToMain(user){//未実装=>タイマー開始の処理
         config.initialPage.classList.add("d-none");//initialPageを非表示にする
-        config.mainPage.append(View.createMainPage());//mainPage作成
+        config.mainPage.append(View.createMainPage(user));//mainPage作成
+    }
+
+    static createInitialUserAccount(userName){//完了
+        let itemsList = [
+            new Items("Flip machine", "ability", 0, 500, 25, 0, 15000, "https://cdn.pixabay.com/photo/2019/06/30/20/09/grill-4308709_960_720.png"),
+            new Items("ETF Stock", "investment", 0, -1, 0, 0.1, 300000, "https://cdn.pixabay.com/photo/2016/03/31/20/51/chart-1296049_960_720.png"),
+            new Items("ETF Bonds", "investment", 0, -1, 0, 0.07, 300000, "https://cdn.pixabay.com/photo/2016/03/31/20/51/chart-1296049_960_720.png"),
+            new Items("Lemonade Stand", "realState", 0, 1000, 30, 0, 30000, "https://cdn.pixabay.com/photo/2012/04/15/20/36/juice-35236_960_720.png"),
+            new Items("Ice Cream Truck", "realState", 0, 500, 120, 0, 100000, "https://cdn.pixabay.com/photo/2020/01/30/12/37/ice-cream-4805333_960_720.png"),
+            new Items("House", "realState", 0, 100, 32000, 0, 20000000, "https://cdn.pixabay.com/photo/2016/03/31/18/42/home-1294564_960_720.png"),
+            new Items("TownHouse", "realState", 0, 100, 64000, 0, 40000000, "https://cdn.pixabay.com/photo/2019/06/15/22/30/modern-house-4276598_960_720.png"),
+            new Items("Mansion", "realState", 0, 20, 500000, 0, 250000000, "https://cdn.pixabay.com/photo/2017/10/30/20/52/condominium-2903520_960_720.png"),
+            new Items("Industrial Space", "realState", 0, 10, 2200000, 0, 1000000000, "https://cdn.pixabay.com/photo/2012/05/07/17/35/factory-48781_960_720.png"),
+            new Items("Hotel Skyscraper", "realState", 0, 5, 25000000, 0, 10000000000, "https://cdn.pixabay.com/photo/2012/05/07/18/03/skyscrapers-48853_960_720.png"),
+            new Items("Bullet-Speed Sky Railway", "realState", 0, 1, 30000000000, 0, 10000000000000, "https://cdn.pixabay.com/photo/2013/07/13/10/21/train-157027_960_720.png")
+        ]
+        if(userName == "cheater") return new User(userName, 20, 0, Math.pow(10,9), itemsList);//所持金が1億$のチートユーザー!?
+        return new User(userName, 20, 0, 50000, itemsList);
     }
 
     /*
-    static createInitialUserAccount(){
-        
-    }
-
     static startTimer(){
         
     }
