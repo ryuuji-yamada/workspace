f6f47199e7c84abb24b597068cb2865f9dc4980d
cassandra <yamada0525@outlook.jp>
2023-03-23 21:26:02 +0900

    practice(object equality)

--- practice(OOP with Java)/src/Battery.java
+++ practice(OOP with Java)/src/Battery.java
@@ -31,21 +31,23 @@ class Battery {
 class Main{
     public static void main(String[] args){
         Battery mc96 = new Battery("VTec", "MC96", 14.4, 6.6, 0.55, 72, 97, 51.5);
-        Battery mc60 = new Battery("VTec", "MC60", 14.4, 4.2, 0.35, 52, 77, 40.5);
-        Battery mdPL140 = new Battery("BowserPower", "MD-PL140", 14.4, 9.9, 1.18, 89, 119, 85);
-        Battery zlD72 = new Battery("MT-Dell Tech", "ZL-D72", 7.2, 9.9, 1.18, 38, 80, 70);
+        Battery mc96Second = mc96;//シャローコピー
+        //↓メンバ変数がすべて同じのディープコピー
+        Battery mc96Third = new Battery("VTec", "MC96", 14.4, 6.6, 0.55, 72, 97, 51.5);
+        Battery mdLs95 = new Battery("Atomic Units", "MD-LS95", 14.4, 6.6, 0.55, 72, 97, 51.5);
 
-        System.out.println("Instance Reference:" + Integer.toHexString(mc96.hashCode()));
         System.out.println(mc96);
+        System.out.println();
+        System.out.println(mc96Second);
+        System.out.println();
+        System.out.println(mc96Third);
+        System.out.println();
+        System.out.println(mdLs95);
 
         System.out.println();
-        System.out.println(mc96.ampHours + " are its amps");
-
-        System.out.println();
-        System.out.println(mc60);
-        System.out.println();
-        System.out.println(mdPL140);
-        System.out.println();
-        System.out.println(zlD72);
+        //System.out.println(mc96 == mc96); //Syntax error
+        System.out.println(mc96 == mc96Second); //True
+        System.out.println(mc96 == mc96Third); //False
+        System.out.println(mc96 == mdLs95); //False
     }
 }
